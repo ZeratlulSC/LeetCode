1eed6ee12475b6f410f1cf9b99fe29d38b880dec
ZeratulSC <nikolai.klokov@gmail.com>
2024-04-08 21:35:25 +0300

    отсортировал файл задачи из общей кучи

---
+++ leetcode_linkedlist.c
@@ -0,0 +1,54 @@
+typedef struct {
+    int val;
+    MyLinkedList next;
+} MyLinkedList;
+
+
+MyLinkedList* myLinkedListCreate() {
+    
+}
+
+int myLinkedListGet(MyLinkedList* obj, int index) {
+    
+}
+
+void myLinkedListAddAtHead(MyLinkedList* obj, int val) {
+    
+}
+
+void myLinkedListAddAtTail(MyLinkedList* obj, int val) {
+    
+}
+
+void myLinkedListAddAtIndex(MyLinkedList* obj, int index, int val) {
+    
+}
+
+void myLinkedListDeleteAtIndex(MyLinkedList* obj, int index) {
+    
+}
+
+void myLinkedListFree(MyLinkedList* obj) {
+    
+}
+
+/**
+ * Your MyLinkedList struct will be instantiated and called as such:
+ * MyLinkedList* obj = myLinkedListCreate();
+ * int param_1 = myLinkedListGet(obj, index);
+ 
+ * myLinkedListAddAtHead(obj, val);
+ 
+ * myLinkedListAddAtTail(obj, val);
+ 
+ * myLinkedListAddAtIndex(obj, index, val);
+ 
+ * myLinkedListDeleteAtIndex(obj, index);
+ 
+ * myLinkedListFree(obj);
+*/
+
+int main (){
+    printf("%d\n", isPalindrome(54321));
+    return 0;
+}
