048fbe3e0667abc16cde672370b2af1d694c86c2
ZeratulSC <nikolai.klokov@gmail.com>
2024-04-11 04:46:45 +0300

    первое приблежение решения

---
+++ gcdofstring/leetcode.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
+
+char* gcdOfStrings(char* str1, char* str2) {
+    int lens1 = strlen(str1);
+    int lens2 = strlen(str2);
+    int maxstr = (lens1>lens2)?1:2;
+    char *ret = (char*)malloc((lens1>lens2)?lens1:lens2 + 1);
+    if (maxstr == 1){
+        if (strstr(str1,str2) == NULL){
+            ret[0] = '\0';
+            return ret;
+        }
+    }
+    else if (maxstr == 2){
+        if (strstr(str2, str1) == NULL){
+            ret[0] = '\0';
+            return ret;
+        }
+    }
+    return ret;
+}
+
+int main (){
+    char note[] = "ABCABC";
+    char mag[] = "ABC";
+    printf("%s\n", strstr(note, mag));
+    printf("%s\n", gcdOfStrings(note, mag));
+    return 0;
+}
