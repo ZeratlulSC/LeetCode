4051309804017a4708cbb2a3d272b33c4f56bafa
ZeratulSC <nikolai.klokov@gmail.com>
2024-04-12 01:34:19 +0300

    чужое решение, мое было неправильным/долгим

--- gcdofstring/leetcode.c
+++ gcdofstring/leetcode.c
@@ -3,30 +3,35 @@
 #include <string.h>
 #include <stdlib.h>
 
-char* gcdOfStrings(char* str1, char* str2) {
-    int lens1 = strlen(str1);
-    int lens2 = strlen(str2);
-    int maxstr = (lens1>lens2)?1:2;
-    char *ret = (char*)malloc((lens1>lens2)?lens1:lens2 + 1);
-    if (maxstr == 1){
-        if (strstr(str1,str2) == NULL){
-            ret[0] = '\0';
-            return ret;
-        }
+int gcd(int a, int b)
+{
+    if(b==0) return a;
+    return gcd(b,a%b);
+}
+char* gcdOfStrings(char* str1, char* str2) 
+{
+    
+    int slen1=strlen(str1);
+    int slen2=strlen(str2);
+    int k = gcd(slen1,slen2);
+    for(int i=0;i<k;++i) if(str1[i]!=str2[i]) return "";
+    for(int i=0;i<=slen1-k;i+=k)
+    {
+        for(int j=0;j<k;++j) if(str1[j]!=str1[i+j]) return "";
     }
-    else if (maxstr == 2){
-        if (strstr(str2, str1) == NULL){
-            ret[0] = '\0';
-            return ret;
-        }
+    for(int i=0;i<=slen2-k;i+=k)
+    {
+        for(int j=0;j<k;++j) if(str2[j]!=str2[i+j]) return "";
     }
-    return ret;
+    char* res = (char*)malloc(sizeof(char)*(k+1));
+    for(int i=0;i<k;++i) res[i]=str1[i];
+    res[k]=0;
+    return res;
 }
 
 int main (){
     char str1[] = "ABCABC";
     char str2[] = "ABC";
-    printf("%s\n", strstr(str1, str2));
     printf("%s\n", gcdOfStrings(str1, str2));
     return 0;
 }
